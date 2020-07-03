5703a12562ca4765facb756c4b925113a67d2090
RafaelM <RafaelM.Science@gmail.com>
2020-07-03 18:14:44 -0400

    Feat: Tratando exceções
    -Lidando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 
 import uploadConfig from './config/upload';
 import './database';
+
+import AppError from './error/AppError';
 
 const app = express();
 
@@ -13,6 +17,22 @@ app.use(express.json());
 // para direcionar as imagens via navegador
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
--- src/services/CreateUserServices.ts
+++ src/services/CreateUserServices.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import User from '../models/User';
 
+import AppError from '../error/AppError';
+
 interface Request {
   name: string;
   email: string;
@@ -17,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email addres alredy used');
+      throw new AppError('Email addres alredy used');
     }
 
     const hashedPassword = await hash(password, 8);
